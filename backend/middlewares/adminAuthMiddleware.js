daddb4e4bacbc84f85453815f4a86ea8af51d61f
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-12-04 10:53:43 +0530

    Admin and User Middlewares for authorization added

--- backend/middlewares/adminAuthMiddleware.js
+++ backend/middlewares/adminAuthMiddleware.js
@@ -7,10 +7,10 @@ const adminAuthMiddleware = (req, res, next)=>{
         //req.headers.authorization: Retrieves the Authorization header from the incoming request .split(' '): Splits the string into an array where each word is separated by a space.[1]: Gets the second element (index 1) of the resulting array, which is the token.
 
         if(!token){
-            throw new Error("Authentication Failed");
+            throw new Error("Authentication Failed!!!");
         }
         const verified = jwt.verify(token, process.env.ADMIN_JWT_SECRET)   //Compares with our token
-        req.adminEmail = verified.email;
+        req.adminEmail = verified.email;      //email from payload (verified) to the req object. This makes the email accessible in subsequent middleware or route handlers
         next();
     } catch (error) {
         return res.status(401).json({success:false, message:error.message, error_code:404, data:{}})
---
+++ backend/middlewares/userAuthMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const userAuthMiddleware = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split("")[1];
+
+    if (!token) {
+      throw new Error("Authorization failed!!!");
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = verified.userId;
+    req.userName = verified.username;
+    next();
+
+  } catch (error) {
+        return res.status(401).json({success:false, message:error.message, error_code:404, data:{}})
+  }
+};
+
+module.exports = {
+    userAuthMiddleware
+}
